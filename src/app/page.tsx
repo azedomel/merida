00a8d93ca59f5222ba5a9de95ad8abf7b165bf53
Kevilin Domingos <kevilinmarcondes356@gmail.com>
2025-04-30 15:51:48 -0300

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+  <>
+  <main className=" w-full">
+  <section className="bg-(--foreground) w-full h-full">
+    <h1 className="flex flex">merida</h1>
+    </section>
+
+  <section>
+
+  </section>
+
+  <section>
+
+  </section>
+  </main>
+  </>
+  );
+}
